4c775e7ef3546932a92a69e5297d091f4df8d002
JCloudYu <j.cloud.yu@gmail.com>
2025-09-22 16:27:14 +0800

    Fix incorrect expired condition detection logic

--- cron.refresh-certificate.ts
+++ cron.refresh-certificate.ts
@@ -63,8 +63,8 @@ dotenv.config({path:['.env', '.env.local', '.env.prod'], override:true});
 			const cert = await fs.readFile(certPath);
 			const certInfo = acme.crypto.readCertificateInfo(cert);
 			const expiredTime = certInfo.notAfter.getTime();
-			const updateBoundary = Date.now() - 7 * 86400_000;
-			const isExpired = expiredTime >= updateBoundary;
+			const updateBoundary = Date.now() + 7 * 86400_000;
+			const isExpired = expiredTime <= updateBoundary;
 			
 			if (!isExpired) {
 				console.log(`${paddedDirName}: ${dayjs(expiredTime).format('YYYY-MM-DD HH:mm')}. Passed! ✅`);
